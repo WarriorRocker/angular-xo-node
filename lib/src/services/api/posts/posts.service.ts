bb675f8f36364cc66c2767e118e6cffd4150d884
Robert Brown <robertbrown@zadv.com>
2019-03-07 18:27:34 -0500

    Clean up unused imports

--- lib/src/services/api/posts/posts.service.ts
+++ lib/src/services/api/posts/posts.service.ts
@@ -1,5 +1,4 @@
 ﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
--- lib/src/services/api/routes/routes.service.ts
+++ lib/src/services/api/routes/routes.service.ts
@@ -1,5 +1,4 @@
 ﻿import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
