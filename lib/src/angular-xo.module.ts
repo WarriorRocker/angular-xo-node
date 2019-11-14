ccd2666542209aecd6dc33cd843e7ba19439a33f
Robert Brown <robertbrown@zadv.com>
2019-11-14 10:59:49 -0500

    Update build scripts to copy readme, changelog, and license files

--- lib/src/angular-xo.module.ts
+++ lib/src/angular-xo.module.ts
@@ -1,29 +1,25 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { XoApiModule } from './services/api/api.module';
-
 import { XoGlobalsModule } from './services/globals/globals.service';
+import { XoApiModule } from './services/api/api.module';
 
 import { XoRouteService } from './services/routes/route.service';
 
 import { XoPostResolver } from './services/posts/post.resolver';
+import { XoTermResolver } from './services/terms/term.resolver';
+import { XoBreadcrumbsResolver } from './services/routes/breadcrumbs.resolver';
 
 import { XoPostComponent } from './components/posts/post.component';
 import { XoPostsListComponent } from './components/posts/postsList.component';
 import { XoPostTermComponent } from './components/posts/postTerm.component';
-
-import { XoTermResolver } from './services/terms/term.resolver';
-
 import { XoTermComponent } from './components/terms/term.component';
-
-import { XoBreadcrumbsResolver } from './services/routes/breadcrumbs.resolver';
 
 @NgModule({
 	imports: [
-		CommonModule,
+		BrowserModule,
 		RouterModule,
 		HttpClientModule,
 		XoGlobalsModule.forBrowser(),
